e0390070633a4f510745a1cd6cf47fd7a96471fe
Jew <22373498@buaa.edu.cn>
2024-11-18 23:59:31 +0800

    ✨ feat(讨论区): 修改回复样式

--- web/src/components/DiscussionContent.vue
+++ web/src/components/DiscussionContent.vue
@@ -35,25 +35,15 @@
       <v-divider></v-divider>
       <v-row no-gutters>
         <v-col cols="auto">
-          <v-btn
-            rounded="0"
-            @click="toggleLike('main')"
-            class="like-btn"
-            :variant="mainDiscussion.isLiked ? 'tonal' : 'text'"
-            :color="'#ee3f4d'"
-          >
+          <v-btn rounded="0" @click="toggleLike('main')" class="like-btn"
+            :variant="mainDiscussion.isLiked ? 'tonal' : 'text'" :color="'#ee3f4d'">
             <v-icon>{{ mainDiscussion.isLiked ? "mdi-thumb-up" : "mdi-thumb-up-outline" }}</v-icon>
             {{ mainDiscussion.isLiked ? "取消点赞" : "点赞" }}
           </v-btn>
         </v-col>
         <v-col cols="auto">
-          <v-btn
-            rounded="0"
-            @click="toggleSubscription"
-            class="subscribe-btn"
-            :variant="isSubscribed ? 'tonal' : 'text'"
-            :color="'#fbc02d'"
-          >
+          <v-btn rounded="0" @click="toggleSubscription" class="subscribe-btn"
+            :variant="isSubscribed ? 'tonal' : 'text'" :color="'#fbc02d'">
             <v-icon>{{ isSubscribed ? "mdi-bell-off" : "mdi-bell-outline" }}</v-icon>
             {{ isSubscribed ? "取消订阅" : "订阅" }}
           </v-btn>
@@ -74,23 +64,25 @@
 
       <!-- 评论区 -->
       <v-divider></v-divider>
-      <div v-for="comment in mainDiscussion.comments" :key="comment.id">
-        <v-card class="mx-auto comment-card" max-width="85%" style="margin-right: auto; margin-left: auto;">
+      <div v-for="(comment, index) in mainDiscussion.comments" :key="comment.id">
+        <v-card rounded="0" class="ml-auto comment-card" variant="text"
+          :style="{ maxWidth: 'calc(100% - 75px)', marginBottom: '0.5px' }">
+          <!-- 评论内容 -->
           <v-card-item :prepend-avatar="comment.avatar">
-            <v-card-item-title class="text-h6 font-weight-regular">
+            <v-card-item-title class="text-subtitle-1 font-weight-regular">
               <v-row align="center" no-gutters>
                 <v-col cols="auto">
                   {{ comment.publisher }}
                   回复
                   <span v-if="comment.targetId">
-                    @{{ getCommentPublisherById(comment.targetId) }}
+                    {{ getCommentPublisherById(comment.targetId) }}
                   </span>
                 </v-col>
               </v-row>
             </v-card-item-title>
             <v-card-item-subtitle>
               <v-row align="center" no-gutters>
-                <v-col cols="auto" class="text-body-1 text-disabled">
+                <v-col cols="auto" class="text-body-2 text-disabled">
                   {{ formatDate(comment.publishTime) }}
                 </v-col>
               </v-row>
@@ -103,16 +95,10 @@
               <v-md-preview :text="comment.content"></v-md-preview>
             </div>
           </v-card-text>
-          <v-divider></v-divider>
           <v-row no-gutters>
             <v-col cols="auto">
-              <v-btn
-                rounded="0"
-                @click="toggleLike(comment.id)"
-                class="like-btn"
-                :variant="comment.isLiked ? 'tonal' : 'text'"
-                :color="'#ee3f4d'"
-              >
+              <v-btn rounded="0" @click="toggleLike(comment.id)" class="like-btn"
+                :variant="comment.isLiked ? 'tonal' : 'text'" :color="'#ee3f4d'">
                 <v-icon>{{ comment.isLiked ? "mdi-thumb-up" : "mdi-thumb-up-outline" }}</v-icon>
                 {{ comment.isLiked ? "取消点赞" : "点赞" }}
               </v-btn>
@@ -123,12 +109,20 @@
                 评论
               </v-btn>
             </v-col>
+            <v-col cols="auto">
+              <v-btn rounded="0" variant="text" :color="'#1867c0'">
+                <v-icon left>mdi-pencil</v-icon>
+                编辑
+              </v-btn>
+            </v-col>
           </v-row>
         </v-card>
+
+        <!-- 在每个 comment-card 后插入 v-divider，除非是最后一个评论 -->
+        <v-divider v-if="index < mainDiscussion.comments.length - 1" class="my-2"></v-divider>
       </div>
     </v-card>
 
-    <!-- 跟随的讨论列表 -->
     <div v-for="discussion in followDiscussion" :key="discussion.id">
       <v-card class="mx-auto follow-card" max-width="85%" style="margin-bottom: 20px;">
         <v-card-item :prepend-avatar="discussion.avatar">
@@ -160,13 +154,8 @@
         <v-divider></v-divider>
         <v-row no-gutters>
           <v-col cols="auto">
-            <v-btn
-              rounded="0"
-              @click="toggleLike(discussion.id)"
-              class="like-btn"
-              :variant="discussion.isLiked ? 'tonal' : 'text'"
-              :color="'#ee3f4d'"
-            >
+            <v-btn rounded="0" @click="toggleLike(discussion.id)" class="like-btn"
+              :variant="discussion.isLiked ? 'tonal' : 'text'" :color="'#ee3f4d'">
               <v-icon>{{ discussion.isLiked ? "mdi-thumb-up" : "mdi-thumb-up-outline" }}</v-icon>
               {{ discussion.isLiked ? "取消点赞" : "点赞" }}
             </v-btn>
@@ -184,6 +173,66 @@
             </v-btn>
           </v-col>
         </v-row>
+
+        <!-- 跟随讨论的评论区 -->
+        <v-divider></v-divider>
+        <div v-for="(comment, index) in discussion.comments" :key="comment.id">
+          <v-card rounded="0" class="ml-auto comment-card" variant="text"
+            :style="{ maxWidth: 'calc(100% - 75px)', marginBottom: '0.5px' }">
+            <!-- 评论内容 -->
+            <v-card-item :prepend-avatar="comment.avatar">
+              <v-card-item-title class="text-subtitle-1 font-weight-regular">
+                <v-row align="center" no-gutters>
+                  <v-col cols="auto">
+                    {{ comment.publisher }}
+                    回复
+                    <span v-if="comment.targetId">
+                      {{ getCommentPublisherById(comment.targetId) }}
+                    </span>
+                  </v-col>
+                </v-row>
+              </v-card-item-title>
+              <v-card-item-subtitle>
+                <v-row align="center" no-gutters>
+                  <v-col cols="auto" class="text-body-2 text-disabled">
+                    {{ formatDate(comment.publishTime) }}
+                  </v-col>
+                </v-row>
+              </v-card-item-subtitle>
+            </v-card-item>
+            <v-card-text>
+              <v-divider style="padding-top: 10px; padding-bottom: 0px"></v-divider>
+              <!-- 评论内容 -->
+              <div style="margin-left: -29px">
+                <v-md-preview :text="comment.content"></v-md-preview>
+              </div>
+            </v-card-text>
+            <v-row no-gutters>
+              <v-col cols="auto">
+                <v-btn rounded="0" @click="toggleLike(comment.id)" class="like-btn"
+                  :variant="comment.isLiked ? 'tonal' : 'text'" :color="'#ee3f4d'">
+                  <v-icon>{{ comment.isLiked ? "mdi-thumb-up" : "mdi-thumb-up-outline" }}</v-icon>
+                  {{ comment.isLiked ? "取消点赞" : "点赞" }}
+                </v-btn>
+              </v-col>
+              <v-col cols="auto">
+                <v-btn rounded="0" variant="text" :color="'#574266'">
+                  <v-icon left>mdi-comment-outline</v-icon>
+                  评论
+                </v-btn>
+              </v-col>
+              <v-col cols="auto">
+                <v-btn rounded="0" variant="text" :color="'#1867c0'">
+                  <v-icon left>mdi-pencil</v-icon>
+                  编辑
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card>
+
+          <!-- 在每个 comment-card 后插入 v-divider，除非是最后一个评论 -->
+          <v-divider v-if="index < discussion.comments.length - 1" class="my-2"></v-divider>
+        </div>
       </v-card>
     </div>
   </div>
@@ -231,6 +280,7 @@ export default {
             publishTime: "2024-09-25T15:30:00",
             lastUpdated: "2024-11-15T15:30:00",
             content: `评论2`,
+            isLiked: false,
           },
           {
             id: 910,
@@ -240,6 +290,7 @@ export default {
             publishTime: "2024-09-25T15:30:00",
             lastUpdated: "2024-11-15T15:30:00",
             content: `评论3`,
+            isLiked: false,
           },
         ],
       },
@@ -253,6 +304,18 @@ export default {
           lastUpdated: "2024-11-15T15:30:00",
           content: "内容1",
           isLiked: false,
+          comments: [
+            {
+              id: 3080,
+              targetId: 3,
+              publisher: "jzz",
+              avatar: "https://randomuser.me/api/portraits/women/41.jpg",
+              publishTime: "2024-09-25T15:30:00",
+              lastUpdated: "2024-11-15T15:30:00",
+              content: `评论2`,
+              isLiked: false,
+            }
+          ]
         },
         {
           id: 30,
@@ -262,6 +325,18 @@ export default {
           lastUpdated: "2024-11-15T15:30:00",
           content: "内容2",
           isLiked: false,
+          comments: [
+            {
+              id: 3082,
+              targetId: 30,
+              publisher: "guishu",
+              avatar: "https://randomuser.me/api/portraits/women/45.jpg",
+              publishTime: "2024-09-25T15:30:00",
+              lastUpdated: "2024-11-15T15:30:00",
+              content: `评论2`,
+              isLiked: false,
+            }
+          ]
         },
       ],
     };
@@ -290,16 +365,42 @@ export default {
     toggleSubscription() {
       this.isSubscribed = !this.isSubscribed;
     },
+    findCommentById(id) {
+      // 查找主讨论的评论
+      let comment = this.mainDiscussion.comments.find(c => c.id === id);
+      if (comment) return comment;
+
+      // 查找跟随讨论的评论
+      for (const discussion of this.followDiscussion) {
+        comment = discussion.comments.find(c => c.id === id);
+        if (comment) return comment;
+      }
+
+      return null;
+    },
+
     toggleLike(discussionId) {
       if (discussionId === "main") {
         this.mainDiscussion.isLiked = !this.mainDiscussion.isLiked;
       } else {
+        // 检查是否是跟随讨论的ID
         const discussion = this.followDiscussion.find(
           (d) => d.id === discussionId
         );
         if (discussion) {
           discussion.isLiked = !discussion.isLiked;
+          return;
         }
+
+        // 查找并切换评论的 isLiked
+        const comment = this.findCommentById(discussionId);
+        if (comment) {
+          comment.isLiked = !comment.isLiked;
+          return;
+        }
+
+        // 如果ID未找到，可以选择抛出错误或忽略
+        console.warn(`未找到对应的讨论或评论，ID: ${discussionId}`);
       }
     },
     getCommentPublisherById(id) {
@@ -307,8 +408,25 @@ export default {
       if (id === this.mainDiscussion.id) {
         return this.mainDiscussion.publisher; // 回复主帖的评论
       }
-      const comment = this.mainDiscussion.comments.find((c) => c.id === id);
-      return comment ? comment.publisher : "未知用户";
+
+      // 查找主讨论的评论
+      const mainComment = this.mainDiscussion.comments.find((c) => c.id === id);
+      if (mainComment) {
+        return mainComment.publisher;
+      }
+
+      // 查找跟随讨论的评论
+      for (const d of this.followDiscussion) {
+        if (d.id === id) {
+          return d.publisher;
+        }
+        const comment = d.comments.find((c) => c.id === id);
+        if (comment) {
+          return comment.publisher;
+        }
+      }
+
+      return "未知用户";
     },
   },
 };
@@ -316,6 +434,7 @@ export default {
 
 <style scoped>
 .scroll-container {
+  flex-direction: column;
   flex: 1 1 auto;
   overflow-y: auto;
   padding: 16px;
